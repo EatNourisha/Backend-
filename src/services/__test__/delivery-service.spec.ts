ca2b7a6fa0ea392f7d44c413f21bd6f3d6afa7bb
famuyiwadayo <famuyiwadayodaniel@gmail.com>
2023-12-17 14:18:55 +0100

    fixed build failure

--- src/services/__test__/delivery-service.spec.ts
+++ src/services/__test__/delivery-service.spec.ts
@@ -1,11 +1,11 @@
 import { DeliveryService } from "../Meal/delivery.service";
 
 test("should have the correct delivery_day", async () => {
-  const info = await DeliveryService.updateDeliveryDayOfWeek("64b67b305a80379a1dbc21b9", "sunday");
+  const info = await DeliveryService.updateDeliveryDayOfWeek("64b67b305a80379a1dbc21b9", "sunday", new Date());
   expect(info.delivery_day).toBe("sunday");
 });
 
 test("should have the correct delivery date", async () => {
-  const info = await DeliveryService.updateDeliveryDayOfWeek("64b67b305a80379a1dbc21b9", "sunday");
+  const info = await DeliveryService.updateDeliveryDayOfWeek("64b67b305a80379a1dbc21b9", "sunday", new Date());
   expect(info.delivery_day).toBe("sunday");
 });
