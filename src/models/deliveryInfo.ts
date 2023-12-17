ac96abb90f29b872bed56b3f0d89d076cfe5bd5a
famuyiwadayo <famuyiwadayodaniel@gmail.com>
2023-12-17 13:36:46 +0100

    fixed delivery date issues

--- src/models/deliveryInfo.ts
+++ src/models/deliveryInfo.ts
@@ -6,22 +6,20 @@ import { Plan } from "./plan";
 
 @modelOptions({ schemaOptions: { timestamps: true } })
 export class DeliveryInfo extends BaseEntity {
-
   @prop({ ref: () => "Customer" })
   customer?: Ref<Customer>;
 
   @prop()
   next_delivery_date?: Date;
 
-  @prop({default: 'sunday'})
+  @prop({ default: "sunday" })
   delivery_day: string;
 
   @prop()
   next_lineup_change_date: Date;
 
-  @prop({default: false})
+  @prop({ default: false })
   is_lineup_change_locked: boolean;
- 
 }
 
 export default getModelForClass(DeliveryInfo);
