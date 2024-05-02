0ec236831e5d309156bb93463438bcc073311013
shukazuby <shukazuby@gmail.com>
2024-05-02 14:25:13 +0100

    use earning balance to purchase single meals

--- src/services/Billing/billing.service.ts
+++ src/services/Billing/billing.service.ts
@@ -1,5 +1,20 @@
 import Stripe from "stripe";
-import { Card, Customer, Order, Plan, PromoCode, Subscription, card, customer, earnings, order, plan,  referral, subscription, transaction } from "../../models";
+import {
+  Card,
+  Customer,
+  Order,
+  Plan,
+  PromoCode,
+  Subscription,
+  card,
+  customer,
+  earnings,
+  order,
+  plan,
+  referral,
+  subscription,
+  transaction,
+} from "../../models";
 import { RoleService } from "../role.service";
 import { createError, epochToCurrentTime, validateFields } from "../../utils";
 import { AvailableResource, PermissionScope } from "../../valueObjects";
@@ -57,6 +72,7 @@ export class BillingService {
 
   // This method creates an intent to collect customer's payment details and then store the payment
   // method so it can be reused later.
+  
   async createSetupIntent(customer_id: string, _: CreateCheckoutSessionDto, roles: string[]) {
     await RoleService.hasPermission(roles, AvailableResource.CUSTOMER, [PermissionScope.READ, PermissionScope.ALL]);
 
@@ -86,78 +102,64 @@ export class BillingService {
 
     if (_order?.total < 1) throw createError("Order must have a price greater than zero", 409);
 
-    // Check if the customer has an earning balance
     const cusBalance = await earnings.findOne({ customer: cus?._id });
 
     let amountToPay = _order.total;
 
     if (cusBalance) {
-        const remainingBalance = cusBalance.balance - _order.total;
-
-        if (remainingBalance >= 0) {
-            amountToPay = 0; // Set amountToPay to 0 if the balance covers the cart total
-        } else {
-            amountToPay = Math.abs(remainingBalance); // Calculate the amount to pay after deducting the balance
-        }
-    }
-    if (_order?.total > 100) {
-        const referrals = await referral.findOne({ invitee: cus?._id }).exec();
-        const isCustomerReferred = await earnings.exists({ refs: cus?._id });
-
-        if (!isCustomerReferred && referrals) {
-            const inviterEarning = await earnings.findOne({ customer: referrals.inviter }).exec();
-            if (inviterEarning) {
-                inviterEarning.balance += 10;
-                inviterEarning.refs.push(cus?._id);
-                await inviterEarning.save();
-            }
-        }
-    }
-
-    const intent = await this.stripe.paymentIntents.create({
-        customer: cus?.stripe_id,
-        payment_method: dto?.card_token,
-        amount: Math.round(amountToPay * 100),
-        currency: "gbp",
-        off_session: !!dto?.card_token,
-        receipt_email: cus?.email,
-        expand: ["invoice"],
-        confirm: !!dto?.card_token,
-    });
-
-
-    if (!!intent.id) {
-        await transaction.create({
-            itemRefPath: "Order",
-            item: _order?._id,
-            currency: intent.currency,
-            order_reference: intent?.id,
-            customer: cus?._id,
-            amount: (intent.amount ?? 0) / 100,
-            reference: intent?.id,
-            reason: TransactionReason.ORDER,
-            stripe_customer_id: cus?.stripe_id,
-        });
-    }
-
-    if (cusBalance) {
       const remainingBalance = cusBalance.balance - _order.total;
 
       if (remainingBalance >= 0) {
-          cusBalance.balance = remainingBalance; // Update the remaining balance
+        amountToPay = 0; // Set amountToPay to 0 if the balance covers the cart total
       } else {
-          cusBalance.balance = 0; // Set the balance to 0
+        amountToPay = Math.abs(remainingBalance); // Calculate the amount to pay after deducting the balance
       }
-      await cusBalance.save(); // Save the updated balance after successful payment
-}
+    }
+    if (_order?.total > 100 && cus?._id) {
+      const referrals = await referral.findOne({ invitee: cus._id }).exec();
+      if (referrals) {
+          await referrals.updateOne({ is_subscribed: true });
+            const isCustomerReferred = await earnings.exists({ refs: cus._id });
+          if (!isCustomerReferred) {
+              const inviterEarning = await earnings.findOne({ customer: referrals.inviter }).exec();
+              if (inviterEarning) {
+                  inviterEarning.balance += 10;
+                  inviterEarning.refs.push(cus._id);
+                  await inviterEarning.save();
+              }
+          }
+      }
+  }
 
+    const intent = await this.stripe.paymentIntents.create({
+      customer: cus?.stripe_id,
+      payment_method: dto?.card_token,
+      amount: Math.round(amountToPay * 100),
+      currency: "gbp",
+      off_session: !!dto?.card_token,
+      receipt_email: cus?.email,
+      expand: ["invoice"],
+      confirm: !!dto?.card_token,
+    });
 
-    console.log("[Initialize Payment]", { dto, client_secret: intent?.client_secret });
+    if (!!intent.id) {
+      await transaction.create({
+        itemRefPath: "Order",
+        item: _order?._id,
+        currency: intent.currency,
+        order_reference: intent?.id,
+        customer: cus?._id,
+        amount: (intent.amount ?? 0) / 100,
+        reference: intent?.id,
+        reason: TransactionReason.ORDER,
+        stripe_customer_id: cus?.stripe_id,
+      });
+    }
+
+    // console.log("[Initialize Payment]", { dto, client_secret: intent?.client_secret });
 
     return { client_secret: intent?.client_secret };
-}
-
-
+  }
 
   // This method creates an intent to collect customer's subscription payment details and then store the payment
   // method so it can be reused later.
@@ -213,88 +215,9 @@ export class BillingService {
       }),
     ]);
 
-    console.log(payment_intent.status)
-    // Check if the customer is an invitee in the referral table
-    const referrals = await referral.findOne({ invitee: cus?._id }).exec();
-  // Check if the customer_id exists in inviter.refs
-   const isCustomerReferred = await earnings.exists({ refs: cus?._id });
-
-    if (!isCustomerReferred && referrals) {
-    // Update the is_subscribed field to true
-      await referrals.updateOne({ is_subscribed: true }).exec();
-
-      // Find the inviter in the earning database
-      const inviterEarning = await earnings.findOne({ customer: referrals.inviter }).exec();
-      if (inviterEarning) {
-        // Reward the inviter, e.g., adding £10 to their earning balance
-        inviterEarning.balance += 10; 
-        inviterEarning.refs.push(cus?._id);
-        await inviterEarning.save();
-      }
-    }
-
     const client_secret = payment_intent.client_secret;
     return { client_secret, subscription_id: sub?.id };
   }
-
-
-  // async initializeSubscription(customer_id: string, dto: InitiateSubscriptionDto, roles: string[]) {
-  //   validateFields(dto, ["plan_id"]);
-  //   await RoleService.hasPermission(roles, AvailableResource.CUSTOMER, [PermissionScope.READ, PermissionScope.ALL]);
-
-  //   const cus = await customer.findById(customer_id).populate("pending_promo").lean<Customer>().exec();
-  //   if (!cus) throw createError("Customer does not exist", 404);
-
-  //   const _plan = await plan.findById(dto?.plan_id).lean<Plan>().exec();
-  //   if (!_plan) throw createError("Plan does not exist", 404);
-
-  //   // one_off allows users to toggle auto charge
-  //   dto.one_off = dto?.one_off ?? true;
-  //   // cancels the subscription when it ends when set to true
-  //   const cancel_at_period_end = !!dto?.one_off || !cus?.preference?.auto_renew;
-
-  //   const promo = cus?.pending_promo as PromoCode;
-  //   const promo_code = when(!!promo && promo?.active === true && !promo?.no_discount, promo?.stripe_id, undefined);
-
-  //   const sub = await this.stripe.subscriptions.create({
-  //     // Possible Break Point
-  //     customer: cus?.stripe_id,
-  //     default_payment_method: dto?.card_token,
-  //     collection_method: "charge_automatically",
-  //     items: [
-  //       {
-  //         price: _plan?.price_id,
-  //         quantity: 1,
-  //       },
-  //     ],
-  //     payment_behavior: "default_incomplete",
-  //     payment_settings: { save_default_payment_method: "on_subscription" },
-  //     expand: ["latest_invoice.payment_intent"],
-  //     cancel_at_period_end,
-  //     promotion_code: promo_code,
-  //   });
-
-  //   const invoice = sub?.latest_invoice as Stripe.Invoice;
-  //   const payment_intent = invoice?.payment_intent as Stripe.PaymentIntent;
-
-  //   await Promise.all([
-  //     transaction.create({
-  //       itemRefPath: "Subscription",
-  //       currency: sub?.currency,
-  //       subscription_reference: sub?.id,
-  //       customer: cus?._id,
-  //       amount: (payment_intent?.amount ?? 0) / 100,
-  //       reference: invoice?.number,
-  //       reason: TransactionReason.SUBSCRIPTION,
-  //       stripe_customer_id: sub?.customer,
-  //     }),
-  //   ]);
-
-
-
-  //   const client_secret = payment_intent.client_secret;
-  //   return { client_secret, subscription_id: sub?.id };
-  // }
 }
 
 export class BillingHooks {
@@ -413,14 +336,40 @@ export class BillingHooks {
       customer.updateOne({ _id: cus?._id }, { subscription_status: data?.status }).lean<Customer>().exec(),
     ]);
 
-    if (data?.status === "active" && !!promo && !!cus?._id) {
-      await Promise.all([
-        !!_plan?._id && DiscountService.updateInfluencersReward(cus?._id!, _plan?._id!, promo),
-        customer.updateOne({ _id: cus?._id }, { pending_promo: null }).exec(),
-      ]);
-    }
+    // if (data?.status === "active" && !!cus?._id) {
+    //   await Promise.all([
+    //     !!_plan?._id && DiscountService.updateInfluencersReward(cus?._id!, _plan?._id!, promo),
+    //     customer.updateOne({ _id: cus?._id }, { pending_promo: null }).exec(),
+    //     ReferralService.updateSubscribersInvite(cus?._id!, _plan?._id!),
+    //   ]);
+    // }
 
-    console.log("Subscription data", { _plan, _card, sub });
+    try {
+      const isCustomerReferred = await earnings.exists({ refs: cus?._id });
+      const referrals = await referral.findOne({ invitee: cus?._id }).exec();
+      const inviterEarning = referrals ? await earnings.findOne({ customer: referrals.inviter }).exec() : null;
+  
+      if (data?.status === "active" && cus?._id) {
+          await Promise.all([
+              _plan?._id && DiscountService.updateInfluencersReward(cus._id, _plan._id, promo),
+  
+              customer.updateOne({ _id: cus._id }, { pending_promo: null }).exec(),
+  
+              !isCustomerReferred && referrals ? referrals.updateOne({ is_subscribed: true }).exec() : null,
+  
+              inviterEarning ? (async () => {
+                  if (!inviterEarning.refs.includes(cus?._id)) {
+                      inviterEarning.balance += 10;
+                      inviterEarning.refs.push(cus?._id);
+                      await inviterEarning.save();
+                  }
+              })() : null,
+          ]);
+      }
+  } catch (error) {
+      console.error("Error:", error);
+  }
+        // console.log("Subscription data", { _plan, _card, sub });
     return sub;
   }
 
