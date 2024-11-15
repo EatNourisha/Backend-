dc5a638ae784d225f17af2d896b4d9182a185011
Shukazuby <shukazuby@gmail.com>
2024-11-15 13:42:09 +0100

    webhook

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -34,7 +34,8 @@ import { sendResponse } from "../utils";
 import config from "../config";
 import { BillingHooks } from "../services";
 // import { authGuard } from "../middlewares";
-import { Customer, Transaction, customer, giftpurchase, transaction, GiftPurchase, promoCode, lineup, order, subscription } from "../models";
+import { Customer, Transaction, customer, giftpurchase, transaction, promoCode, lineup, order, subscription } from "../models";
+// import { Customer, Transaction, customer, giftpurchase, transaction, GiftPurchase, promoCode, lineup, order, subscription } from "../models";
 import { TransactionStatus } from "../models/transaction";
 
 import AppUpdate from "./appupdate.routes";
@@ -114,101 +115,210 @@ routes.post("/webhook", bodyParser.raw({ type: "application/json" }), async (req
       await BillingHooks.paymentIntentCreated(event);
       break;
     }
-    case "payment_intent.succeeded": {
-      const data = event.data.object as any;
-      const customerId = data?.customer;
-      const cus = await customer.findOne({ stripe_id: customerId });
-      const tx = await transaction
-        .findOneAndUpdate({ reference: data?.id, stripe_customer_id: data?.customer }, { status: TransactionStatus.SUCCESSFUL })
-        .lean<Transaction>()
-        .exec();
+    // case "payment_intent.succeeded": {
+    //   const data = event.data.object as any;
+    //   const customerId = data?.customer;
+    //   const cus = await customer.findOne({ stripe_id: customerId });
+    //   const tx = await transaction
+    //     .findOneAndUpdate({ reference: data?.id, stripe_customer_id: data?.customer }, { status: TransactionStatus.SUCCESSFUL })
+    //     .lean<Transaction>()
+    //     .exec();
 
-      if (tx?.reason === "Gift-Card"|| "Custom-Gift") {
-      const gift = await giftpurchase
-          .findOneAndUpdate({ customer: cus?._id, reference: data?.id }, { status: GiftStatus.ACTIVE })
-          .lean<GiftPurchase>()
-          .exec();
+    //   if (tx?.reason === "Gift-Card"|| "Custom-Gift") {
+    //   const gift = await giftpurchase
+    //       .findOneAndUpdate({ customer: cus?._id, reference: data?.id }, { status: GiftStatus.ACTIVE })
+    //       .lean<GiftPurchase>()
+    //       .exec();
 
-          if(gift){
-            await sendGiftBought(cus?.email!, gift )
-            if(gift?.scheduled === false && gift?.scheduled_Email === false){
-              await sendGiftRecipient(gift?.reciever_email!, gift)
-              await sendGiftSent(cus?.email!, gift)
-              await giftpurchase.findOneAndUpdate({_id: gift?._id}, {scheduled_Email: true}).lean<GiftPurchase>()
-              .exec()
-            }}
+    //       if(gift){
+    //         await sendGiftBought(cus?.email!, gift )
+    //         if(gift?.scheduled === false && gift?.scheduled_Email === false){
+    //           await sendGiftRecipient(gift?.reciever_email!, gift)
+    //           await sendGiftSent(cus?.email!, gift)
+    //           await giftpurchase.findOneAndUpdate({_id: gift?._id}, {scheduled_Email: true}).lean<GiftPurchase>()
+    //           .exec()
+    //         }}
 
-      }
+    //   }
 
-      const trans = await transaction.findOne({subscription_reference: data?.id, stripe_customer_id: data?.customer}).lean<Transaction>().exec();
+    //   const trans = await transaction.findOne({subscription_reference: data?.id, stripe_customer_id: data?.customer}).lean<Transaction>().exec();
 
-      if (trans?.applied_promo !== null) {
-        const promo = await promoCode.findById(trans?.applied_promo).exec();
+    //   if (trans?.applied_promo !== null) {
+    //     const promo = await promoCode.findById(trans?.applied_promo).exec();
 
-      if (promo) {
-      await promoCode.updateOne({ $push: { redeemed_by: cus?._id }}).exec();
-          // const updatedRedemptions = Math.max((promo.max_redemptions || 0) - 1, 0);
-          // await promoCode.updateOne({ _id: promo?._id }, { max_redemptions: updatedRedemptions }).exec();
-      }
-      }
+    //   if (promo) {
+    //   await promoCode.updateOne({ $push: { redeemed_by: cus?._id }}).exec();
+    //       // const updatedRedemptions = Math.max((promo.max_redemptions || 0) - 1, 0);
+    //       // await promoCode.updateOne({ _id: promo?._id }, { max_redemptions: updatedRedemptions }).exec();
+    //   }
+    //   }
 
-      const meta = data.metadata;
+    //   const meta = data.metadata;
 
-      if(meta.couponCode !== null || ""){       
-        await giftpurchase.findOneAndUpdate({ code: meta.couponCode }, { status: GiftStatus.REDEEMED, redeemed_by: cus?._id })
-        .lean<GiftPurchase>()
-        .exec();
-    }
+    //   if(meta.couponCode !== null || ""){       
+    //     await giftpurchase.findOneAndUpdate({ code: meta.couponCode }, { status: GiftStatus.REDEEMED, redeemed_by: cus?._id })
+    //     .lean<GiftPurchase>()
+    //     .exec();
+    // }
 
-    if (trans.status === 'successful' && trans.itemRefPath === 'Order') {
-      if (cus) {
-        cus.newUser = false
-        // Initialize or update POSTSUBEEMAILS with default values (all false)
-        cus.POSTSUBEEMAILS = Object.assign({
-          postsub0: false, postsub1: false, postsub2: false, postsub3: false,
-          postsub4: false, postsub5: false, postsub6: false,
-          postsub7: false, postsub8: false, postsub9: false,
-          postsub10: false, postsub11: false,
-          postsub12: false, postsub13: false
-        }, cus.POSTSUBEEMAILS || {});
+    // if (trans.status === 'successful' && trans.itemRefPath === 'Order') {
+    //   if (cus) {
+    //     cus.newUser = false
+    //     // Initialize or update POSTSUBEEMAILS with default values (all false)
+    //     cus.POSTSUBEEMAILS = Object.assign({
+    //       postsub0: false, postsub1: false, postsub2: false, postsub3: false,
+    //       postsub4: false, postsub5: false, postsub6: false,
+    //       postsub7: false, postsub8: false, postsub9: false,
+    //       postsub10: false, postsub11: false,
+    //       postsub12: false, postsub13: false
+    //     }, cus.POSTSUBEEMAILS || {});
   
-        // Initialize or update CARTEMAILS with default values (all false)
-        cus.CARTEMAILS = Object.assign({
-          cart1: false, cart2: false, cart3: false,
-          cart4: false, cart5: false, cart6: false,
-          cart7: false, cart8: false, cart9: false,
-          cart10: false, cart11: false
-        }, cus.CARTEMAILS || {});
+    //     // Initialize or update CARTEMAILS with default values (all false)
+    //     cus.CARTEMAILS = Object.assign({
+    //       cart1: false, cart2: false, cart3: false,
+    //       cart4: false, cart5: false, cart6: false,
+    //       cart7: false, cart8: false, cart9: false,
+    //       cart10: false, cart11: false
+    //     }, cus.CARTEMAILS || {});
   
-        // Initialize or update REENGAGEEMAILS with default values (all false)
-        cus.REENGAGEEMAILS = Object.assign({
-          reengage1: false, reengage2: false, reengage3: false,
-          reengage4: false, reengage5: false, reengage6: false
-        }, cus.REENGAGEEMAILS || {});  
+    //     // Initialize or update REENGAGEEMAILS with default values (all false)
+    //     cus.REENGAGEEMAILS = Object.assign({
+    //       reengage1: false, reengage2: false, reengage3: false,
+    //       reengage4: false, reengage5: false, reengage6: false
+    //     }, cus.REENGAGEEMAILS || {});  
     
-        cus.newUser = false
-        await cus.save();
-      }  
+    //     cus.newUser = false
+    //     await cus.save();
+    //   }  
 
-      if(cus){
-        cus.newUser = false
-        await cus?.save()
-      }
+    //   if(cus){
+    //     cus.newUser = false
+    //     await cus?.save()
+    //   }
 
-    }  
+    // }  
   
 
-    axios.post('https://hooks.zapier.com/hooks/catch/3666010/2mesl25/')
-    .then(response => {
-      console.log('ZAPIER EVENT', response.data);
-    })
-    .catch(error => {
-      console.log('There was an error making the request!', error);
-    });
+    // axios.post('https://hooks.zapier.com/hooks/catch/3666010/2mesl25/')
+    // .then(response => {
+    //   console.log('ZAPIER EVENT', response.data);
+    // })
+    // .catch(error => {
+    //   console.log('There was an error making the request!', error);
+    // });
 
-      await BillingHooks.paymentIntentSucceeded(tx, event);
+    //   await BillingHooks.paymentIntentSucceeded(tx, event);
+    //   break;
+    // }
+
+    case "payment_intent.succeeded": {
+      try {
+        const data = event.data.object as any;
+        const customerId = data?.customer;
+    
+        const cus = await customer.findOne({ stripe_id: customerId });
+        const tx = await transaction
+          .findOneAndUpdate(
+            { reference: data?.id, stripe_customer_id: data?.customer },
+            { status: TransactionStatus.SUCCESSFUL }
+          )
+          .exec();
+              
+
+          if (tx?.reason as unknown === "Gift-Card" || tx?.reason as unknown === "Custom-Gift") {
+            const gift = await giftpurchase
+            .findOneAndUpdate(
+              { customer: cus?._id, reference: data?.id },
+              { status: GiftStatus.ACTIVE }
+            )
+            .exec();
+    
+          if (gift) {
+            await sendGiftBought(cus?.email!, gift);
+    
+            if (!gift.scheduled && !gift.scheduled_Email) {
+              await sendGiftRecipient(gift.reciever_email!, gift);
+              await sendGiftSent(cus?.email!, gift);
+              await giftpurchase
+                .findOneAndUpdate(
+                  { _id: gift._id },
+                  { scheduled_Email: true }
+                )
+                .exec();
+            }
+          }
+        }
+    
+        const trans = await transaction
+          .findOne({
+            subscription_reference: data?.id,
+            stripe_customer_id: data?.customer,
+          })
+          .exec();
+    
+        if (trans?.applied_promo) {
+          const promo = await promoCode.findById(trans.applied_promo).exec();
+          if (promo) {
+            await promoCode.updateOne(
+              { _id: promo._id },
+              { $push: { redeemed_by: cus?._id } }
+            ).exec();
+          }
+        }
+    
+        const meta = data.metadata;
+        if (meta.couponCode) {
+          await giftpurchase
+            .findOneAndUpdate(
+              { code: meta.couponCode },
+              { status: GiftStatus.REDEEMED, redeemed_by: cus?._id }
+            )
+            .exec();
+        }
+    
+        if (trans?.status === "successful" && trans.itemRefPath === "Order") {
+          if (cus) {
+            cus.newUser = false;
+            cus.POSTSUBEEMAILS = {
+              ...Array(14).fill(false).reduce((acc, _, idx) => ({
+                ...acc,
+                [`postsub${idx}`]: false,
+              })),
+              ...cus.POSTSUBEEMAILS,
+            };
+            cus.CARTEMAILS = {
+              ...Array(11).fill(false).reduce((acc, _, idx) => ({
+                ...acc,
+                [`cart${idx + 1}`]: false,
+              })),
+              ...cus.CARTEMAILS,
+            };
+            cus.REENGAGEEMAILS = {
+              ...Array(6).fill(false).reduce((acc, _, idx) => ({
+                ...acc,
+                [`reengage${idx + 1}`]: false,
+              })),
+              ...cus.REENGAGEEMAILS,
+            };
+    
+            await cus.save();
+          }
+        }
+    
+        await axios
+          .post("https://hooks.zapier.com/hooks/catch/3666010/2mesl25/")
+          .then((response) => console.log("ZAPIER EVENT", response.data))
+          .catch((error) =>
+            console.error("There was an error making the request!", error)
+          );
+    
+        await BillingHooks.paymentIntentSucceeded(tx as Transaction, event);
+      } catch (error) {
+        console.error("Error handling payment_intent.succeeded:", error);
+      }
       break;
     }
+    
     case "invoice.paid": {
       await BillingHooks.invoicePaid(event);
       break;
