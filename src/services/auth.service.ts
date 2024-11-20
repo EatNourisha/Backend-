07ea89b753d89f02e5d65f65c193bbd5fc4260a5
Shukazuby <shukazuby@gmail.com>
2024-11-20 18:09:37 +0100

    error

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -35,10 +35,7 @@ export class AuthService {
         acc?.roles 
       );  
     }
-  }    
-
-  acc.lastLoggedPlatform = data?.platform
-  await acc.save()
+  }
   await NourishaBus.emit("customer:logged_in", { owner: acc });
     return { payload, token };
   }
